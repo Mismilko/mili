677e0575b3804de01776151d5583658544475a99
M.I.S <irina.morozova.21@inbox.ru>
2023-05-10 10:58:53 +0300

    change

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+
+    { int buy= 13678;
+        int mile= 20;
+        int bonus = buy/mile;
+        System.out.println("начисленный бонус: " + bonus + " миль");
+    }
+}
